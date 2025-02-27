414cd992be56cd3309791858b6deacf64fd955d5
Shemrei Marabillo <s.marabillo.dev@gmail.com>
2025-02-27 09:47:06 +0800

    commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     <nav className="fixed text-white p-5 top-0 w-full z-1000">
       <div className="nav-content-wrapper flex justify-between items-center p-2 ">
         <div className="logo">Shemrei Marabillo</div>
-        <ul className="nav-links flex gap-14"> 
+        <ul className="nav-links gap-14 hidden md:flex"> 
           <li><a href="">Works</a></li>
           <li><a href="">Services</a></li>
           <li><a href="">About</a></li>
-          <li><a href="">Contact</a></li>
+          <li><a href="" className="">Contact</a></li>
         </ul>
         <a href="">Let's talk <ArrowUpRight className="w-5 h-5" /></a>
       </div>
